fbecf601e4c1aabfb862fea2db88bf32da167d79
Luke Kirby <lbkirby96@hotmail.com>
2022-04-04 16:15:59 +0100

    formatting

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,15 +1,10 @@
-import React from "react";
-
-type Props = {title:string}
-function Title(props:Props) {
-    return (
-
-        <div>
-        <h1>
-           {props.title}
-        </h1>
-        </div>
-    );
+type Props = { title: string };
+function Title(props: Props) {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+    </div>
+  );
 }
 
 export default Title;

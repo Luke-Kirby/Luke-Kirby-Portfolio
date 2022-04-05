172578b9da7b28a1bea91344dcacc047c7cf0764
Luke Kirby <lbkirby96@hotmail.com>
2022-04-05 12:57:27 +0100

    SideBar div (no content)

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import Title from "./components/Title";
 import { Route, Router, Routes, Link } from "react-router-dom";
 import Home from "./view/Home";
 import About from "./view/About";
+import SideBar from './view/SideBar'
 
 function App() {
   return (
     <div className="App">
+            <SideBar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
       </Routes>
+
       <header className="App-header">
         <Link to={"/"}>
           Home
@@ -21,6 +24,9 @@ function App() {
           About
         </Link>
       </header>
+
+
+      
     </div>
   );
 }

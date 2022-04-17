68c5c8e6dff34240b6570d0dd5f5b3d8196765b0
Luke Kirby <lbkirby96@hotmail.com>
2022-04-17 21:17:36 +0100

    update

--- src/components/SideBar/MenuList/MenuList.tsx
+++ src/components/SideBar/MenuList/MenuList.tsx
@@ -1,47 +1,38 @@
 import React from "react";
 import { isTemplateSpan } from "typescript";
 
+type ListItem = {
+  label: string;
+  path: string;
+  icon: string;
+};
+
 type Props = {
-  items: string[];
+  items: ListItem[];
 };
 
 function MenuList(props: Props) {
   return (
     <div>
-
-
-
-      
+      {/* Label */}
       <p
-           style={{
-            fontFamily: "revert",
-            color: "rgb(255, 255, 255)",
-            fontSize: "50px",
-            margin: "auto",
-            marginTop: "10%",
-          }}
-        >
-          {props.items[0]}</p>
-          <p
-           style={{
-            fontFamily: "revert",
-            color: "rgb(255, 255, 255)",
-            fontSize: "50px",
-            margin: "auto",
-            marginTop: "10%",
-          }}
-        >
-          {props.items[1]}</p>
-          <p
-           style={{
-            fontFamily: "revert",
-            color: "rgb(255, 255, 255)",
-            fontSize: "50px",
-            margin: "auto",
-            marginTop: "10%",
-          }}
-        >
-          {props.items[2]}</p>
+        style={{
+          fontFamily: "revert",
+          color: "rgb(255, 255, 255)",
+          fontSize: "30px",
+          margin: "auto",
+          marginTop: "10%",
+        }}
+      >
+        {props.items[0]}
+      </p>
+
+      {/* Icon */}
+      {/* <img
+      alt="Profile Picture"
+      src="/images/icon/houseIcon.jpeg"
+
+    /> */}
     </div>
   );
 }
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -2,11 +2,17 @@ import ProfileDisplay from "./ProfileDisplay";
 import MenuList from "./MenuList";
 import "./SideBar.scss";
 
+
 function SideBar() {
   return (
     <div className="SideBar_main">
       <ProfileDisplay sizePx={200} />
-      <MenuList items={["label", "path","icon"]} />;
+
+
+      <MenuList items={[{
+        label:"test", icon:"", path:""
+      }]}
+      />;
     </div>
   );
 }
